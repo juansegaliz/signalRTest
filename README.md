41bd837d86fcff4079a71196105a393a9f1d91c9
juansegaliz <juansegaliz@gmail.com>
2024-02-27 07:57:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Testing WebSocket with SignalR
+
+This project aims to create a simple hub and test the connection of a WebSocket with SignalR and make sure it works correctly.
+
+## Description
+
+This project uses:
+
+- .NET 6
+- SignalR
+
+## Live test
+
+[You can test it by clicking here](https://signalrtest.juansegaliz.com/index.html)
---
+++ SignalRTest/wwwroot/chat.js
@@ -0,0 +1,17 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const li = document.createElement("li");
+    li.textContent = `${user}: ${message}`;
+    document.getElementById("messages").appendChild(li);
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+function sendMessage() {
+    const user = document.getElementById("user").value;
+    const message = document.getElementById("message").value;
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+}
